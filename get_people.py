a4a4021839920e51177454b71753059b4240b808
Maxim Vasilkin <mxvas@mail.ru>
2022-11-23 21:32:02 +0400

    add bot keyboard, some fix

--- get_people.py
+++ get_people.py
@@ -1,6 +1,8 @@
 import vk_api
+from vk_api.keyboard import VkKeyboard, VkKeyboardColor
 from time import sleep
 from datetime import datetime
+from random import randrange
 
 
 def authorize(path, my_token=False, bot_token=False):
@@ -15,10 +17,33 @@ def authorize(path, my_token=False, bot_token=False):
     with open(path, 'r', encoding='utf-8') as file:
         user_token, public_token = file.readlines()
     if my_token:
-        TOKEN = user_token
+        return vk_api.VkApi(token=user_token, api_version='5.131').get_api()
     elif bot_token:
-        TOKEN = public_token
-    return vk_api.VkApi(token=TOKEN, api_version='5.131').get_api()
+        return vk_api.VkApi(token=public_token, api_version='5.131')
+
+
+def get_user_info(user_name_or_id, my_token_api_object):
+    '''
+    :param user_name_or_id: id или имя пользователя, например: 1 или paveldurov
+    :param my_token_api_object: объект, возвращенный от функции authorize(path, my_token=True)
+    :return: возвращает кортеж данных о пользователе: возраст, город, пол
+    '''
+    sleep(DELAY)
+    user_info = my_token_api_object.users.get(**{'user_ids': user_name_or_id,
+                                                 'fields': 'bdate, city, sex'})[0]
+    user_birthday = user_info.get('bdate', None) # str
+
+    if not user_birthday:
+        user_birthday = input('введите дату рождения, например 10.10.1990: ')   # это надо адаптировать под бота
+
+    user_age = datetime.now().year - int(user_birthday[-4:])
+    user_city_title = user_info.get('city', {}).get('title', None) # str
+
+    if not user_city_title:
+        user_city_title = input('Введите Ваш город: ')  # это надо адаптировать под бота
+
+    user_sex = user_info['sex']  # int: 1 - женщина, 2 - мужчина
+    return user_age, user_city_title, user_sex
 
 
 
@@ -26,21 +51,9 @@ def find_people(user_name_or_id, my_token_api_object):
     '''
     :param user_name_or_id: id или имя пользователя, например: 1 или paveldurov
     :param my_token_api_object: объект, возвращенный от функции authorize(path, my_token=True)
-    :return: id того, кто написал боту, и список найденных ему людей
+    :return: список найденных людей
     '''
-
-    sleep(DELAY)
-    user_info = my_token_api_object.users.get(**{'user_ids': user_name_or_id,
-                                      'fields': 'bdate, city, sex'})[0]
-    user_id = user_info['id']  # int
-    user_birthday = user_info.get('bdate', None) # str
-    user_age = datetime.now().year - int(user_birthday[-4:])
-    user_city_title = user_info.get('city', {}).get('title', None) # str
-    user_sex = user_info['sex']  # int: 1 - женщина, 2 - мужчина
-
-    if not user_city_title:
-        user_city_title = input('Введите Ваш город: ')  # это надо адаптировать под бота
-
+    user_age, user_city_title, user_sex = get_user_info(user_name_or_id, my_token_api_object)
     sleep(DELAY)
     response = my_token_api_object.users.search(**{'sort': '0',
                                                     'count': '1000',
@@ -51,17 +64,26 @@ def find_people(user_name_or_id, my_token_api_object):
                                                     'age_to': str(user_age + 3),     # ищем анкеты +-3 года от возраста пользователя
                                                     'has_photo': '1'})             # строго с фото
     people = response['items'] # тут список найденных людей
-    return user_id, people
+    return people
 
 
-def create_message_for_bot(user_id, list_of_people, my_token_api_object):
+def create_keyboard():
+    keyboard = VkKeyboard()
+    keyboard.add_button('Следующая анкета', color=VkKeyboardColor.POSITIVE)
+    keyboard.add_button('Добавить в избранное', color=VkKeyboardColor.PRIMARY)
+    keyboard.add_button('Открыть избранное', color=VkKeyboardColor.SECONDARY)
+    return keyboard.get_keyboard()
+
+
+
+def create_message_for_bot(user_id, my_token_api_object):
     '''
     :param user_id: тут id того, кто написал боту
     :param list_of_people: список найденных людей для знакомства
     :param my_token_api_object: объект апи, возвращаемый функцией authorize
     :return: возвращает генератор ПАРАМЕТРОВ сообщений. Параметры для ВК метода message.send, это для бота
     '''
-
+    list_of_people = find_people(user_id, my_token_api_object)
     for person in list_of_people:
         if not person['is_closed']:  # closed - закрытый профиль, фотки у такого не собрать
             person_name = f'{person["first_name"]} {person["last_name"]}'
@@ -77,12 +99,12 @@ def create_message_for_bot(user_id, list_of_people, my_token_api_object):
             avatars.sort(key=lambda x: x['likes']['count'])
             avatars = avatars[-3:]
             three_most_liked = [f'photo{photo["owner_id"]}_{photo["id"]}' for photo in avatars] # [max(photo['sizes'], key=lambda x: x['width'])['url'] for photo in avatars]
-            attachment = ', '.join(three_most_liked)
+            attachment = ','.join(three_most_liked)
             params_of_message_send = {'user_id': user_id,
                                       'message': message,
                                       'attachment': attachment,
-                                  #   'keyboard': ''  тут сделаем кнопку для бота
-                                      }
+                                      'random_id': randrange(10 ** 7),
+                                      'keyboard': create_keyboard()}
             yield params_of_message_send
 
             # next_person = input('Открыть следующую анкету? \n1 - Да\n2 - Нет\n')
@@ -94,9 +116,7 @@ DELAY = 0.34  # задержка перед запросом к апи
 
 if __name__ == '__main__':
     user_api_object = authorize('tokens.ini', my_token=True)  # тут объект, созданный на основе личного токена. От него будут апи-запросы, вроде users.search, photos.get
-
-    user_id, list_of_people = find_people('1', user_api_object) # тут id того, кто написал боту, и список найденных ему людей
-    generator_of_messages = create_message_for_bot(user_id, list_of_people, user_api_object)
+    generator_of_messages = create_message_for_bot('1', user_api_object)
 
     for message_params in generator_of_messages:
         print(message_params)
