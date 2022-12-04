0d8a1b69fe3b9b4a0914813f73258c5922116503
Pavel Timofeev <pashatim@yandex.ru>
2022-12-04 21:09:50 +0300

    add main with bot

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-# import
-# import
-# import
-# import
-# import
-#
-#
-# if __name__ == '__main__':
+from vkinder_bot import bot
+from dbdelirium.configinator import public_token, dbpassword, user_token
+
+if __name__ == '__main__':
+    # with open('tokens.ini', 'r', encoding='utf-8') as file:
+    #     user_token, public_token = file.readlines()
+    bot(user_token=user_token, public_token=public_token, db_password=dbpassword)
